3cd8556a1baae822c7050dea49f5c8e98808545d
WooSeok Kim <kws60000@naver.com>
2023-03-20 08:50:11 +0900

    TIL: 엔티티 매니저 팩토리 사용

---
+++ ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
@@ -0,0 +1,41 @@
+package hellojpa;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+
+public class JpaMain {
+
+    public static void main(String[] args) {
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
+
+        EntityManager em = emf.createEntityManager();
+
+        EntityTransaction tx = em.getTransaction();
+
+        tx.begin();
+
+        try {
+            Member findMember = em.find(Member.class, 1L);
+            findMember.setName("HelloJPA");
+
+//            em.persist(findMember);
+
+            System.out.println("findMember = " + findMember.getId());
+            System.out.println("findMember = " + findMember.getName());
+//            Member member = new Member();
+//            member.setId(1L);
+//            member.setName("HelloA");
+
+//            em.persist(member);
+
+            tx.commit();
+        } catch (Exception e) {
+            tx.rollback();
+        } finally {
+            em.close();
+        }
+        emf.close();
+    }
+}
