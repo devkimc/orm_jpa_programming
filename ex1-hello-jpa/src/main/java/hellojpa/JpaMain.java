398ac87a066393ffc7488c3969ff9253884e21fd
WooSeok Kim <kws60000@naver.com>
2023-03-20 23:31:39 +0900

    TIL: 영속성 컨텍스트

--- ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
+++ ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
@@ -4,6 +4,7 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
+import java.util.List;
 
 public class JpaMain {
 
@@ -17,18 +18,10 @@ public class JpaMain {
         tx.begin();
 
         try {
-            Member findMember = em.find(Member.class, 1L);
-            findMember.setName("HelloJPA");
-
-//            em.persist(findMember);
-
-            System.out.println("findMember = " + findMember.getId());
-            System.out.println("findMember = " + findMember.getName());
-//            Member member = new Member();
-//            member.setId(1L);
-//            member.setName("HelloA");
-
-//            em.persist(member);
+            //영속
+            Member member = em.find(Member.class, 150L);
+            member.setName("ZZZZZ");
+            System.out.println("==================");
 
             tx.commit();
         } catch (Exception e) {
